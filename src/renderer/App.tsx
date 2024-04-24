46fac14b225fd64d34aaeab3e889dfd85026b6f5
Lucas Corey <89759400+ArgentumRhodon@users.noreply.github.com>
2024-04-24 14:51:30 -0400

    added pages structure

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,17 +1,12 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-
+import { Login } from './pages';
 import 'tailwindcss/tailwind.css';
-
-function Hello() {
-  return <h1 className="btn">Click Me!</h1>;
-}
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </Router>
   );
